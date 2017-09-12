c2e2dcf63ea821588856488a3a8b0d8a11b8063b
zhangzhen2017 <1565948662@qq.com>
2017-09-12 20:31:21 +0800

    九九乘法表

---
+++ jiujiu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+for i in range(1,10):
+    for j in range(1,i+1):
+        print"%d*%d=%2d"%(i,j,i*j),
+    print (" ")
+
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print"%d*%d=%2d"%(i,j,i*j),
+    print(" ")
+
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print"%d*%d=%2d"%(i,j,i*j),
+    print(" ")
